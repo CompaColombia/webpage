7d9015fdc07a3d55a8f146e17085e86337af27d7
Harold <harold.murcia@nordstrom.com>
2023-11-07 16:17:11 -0500

    version

--- compa-webpage/src/App.js
+++ compa-webpage/src/App.js
@@ -1,19 +1,33 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-
-import Home from './Home'
-import LoadingPage from './LoadingPage';
+import Navbar from './components/Navbar';
+import Agro from './pages/Agro';
+import Tech from './pages/Tech';
+import Loading from './pages/Loading';
+import Footer from './components/Footer';
+import './styles/styles.scss';
+import Home from './pages/Home';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
-  const [allIsFetched, setAllIsFetched] = useState(false)
-  
+
+  const path = useLocation().pathname;
+  const loadingPages = ['/edu', '/mercado']
+
   return (
-    <div className="App">
-      {!allIsFetched && <LoadingPage />}
-      <header className="App-header">
-        <Home setAllIsFetched={() => setAllIsFetched(true)}/>
-      </header>
-    </div>
+    <>
+      <Navbar path={path} />
+      {loadingPages.includes(path) ? <Loading /> :
+        <>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/agro' element={<Agro />} />
+            <Route path='/tech' element={<Tech />} />
+            <Route path='/edu' element={<Agro />} />
+            <Route path='/mercado' element={<Agro />} />
+          </Routes>
+          <Footer />
+        </>
+      }
+    </>
   );
 }
 
